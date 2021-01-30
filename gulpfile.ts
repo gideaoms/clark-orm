e33a7d234cfbe276995d343e5f8e37e71311584f
= <=>
2021-01-30 19:40:52 -0300

    update

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,9 +4,13 @@ import replace from 'gulp-replace';
 
 const compile = () => gulp.src('package.json').pipe(shell('npm run compile', { quiet: true }));
 
+const copy1 = () => gulp.src(['README.md']).pipe(gulp.dest('dist'));
+
+const copy2 = () => gulp.src(['src/resources/ace-manifest.json']).pipe(gulp.dest('dist/resources'));
+
 const update = () =>
   gulp.src('package.json').pipe(replace('"private": true,', '"private": false,')).pipe(gulp.dest('dist'));
 
 const publish = () => gulp.src('package.json').pipe(shell('cd dist && npm publish'));
 
-export default gulp.series(compile, update, publish);
+export default gulp.series(compile, copy1, copy2, update, publish);
--- src/utils/ace.ts
+++ src/utils/ace.ts
@@ -6,28 +6,36 @@ import webApp from '../utils/web-app';
 
 register({ transpileOnly: true });
 
-const main = async (args: string[]) => {
-  console.log(webApp);
-  const kernel = new Kernel(webApp);
-  const manifestLoader = new ManifestLoader([
-    {
-      manifestAbsPath: resolve(__dirname, '..', 'resources', 'ace-manifest.json'),
-      basePath: resolve(__dirname),
-    },
-  ]);
-  kernel.flag(
-    'help',
-    async (value, _, command) => {
-      if (!value) {
-        return;
-      }
-      kernel.printHelp(command);
-      process.exit(0);
-    },
-    { alias: 'h' }
-  );
-  kernel.useManifest(manifestLoader);
-  await kernel.handle(args);
+const kernel = new Kernel(webApp);
+
+const printHelp = (value: any, command?: any) => {
+  if (!value) {
+    return;
+  }
+  kernel.printHelp(command);
+  process.exit(0);
+};
+
+const main = async (argv: string[]) => {
+  try {
+    const manifestLoader = new ManifestLoader([
+      {
+        manifestAbsPath: resolve(__dirname, '..', 'resources', 'ace-manifest.json'),
+        basePath: resolve(__dirname),
+      },
+    ]);
+    kernel.flag('help', async (value, _, command) => printHelp(value, command), { alias: 'h' });
+    kernel.useManifest(manifestLoader);
+    await kernel.preloadManifest();
+    if (!argv.length) {
+      printHelp(true);
+      return;
+    }
+    await kernel.handle(argv);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 main(process.argv.slice(2)).catch(console.error);
