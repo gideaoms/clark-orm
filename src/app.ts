7fc070346df6bd2be8d859d53aff363519f8b87d
Gideão <gideaoms@gmail.com>
2024-08-02 16:18:17 -0300

    new version

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,29 @@
-import { Logger } from '@adonisjs/logger';
-import { Profiler } from '@adonisjs/profiler';
-import { Ioc } from '@adonisjs/fold';
-import { Emitter } from '@adonisjs/events/build/src/Emitter/index.js';
-import { Database } from '@adonisjs/lucid/build/src/Database/index.js';
-import { BaseModel } from '@adonisjs/lucid/build/src/Orm/BaseModel/index.js';
-import { Adapter } from '@adonisjs/lucid/build/src/Orm/Adapter/index.js';
-import { ModelPaginator } from '@adonisjs/lucid/build/src/Orm/Paginator/index.js';
-import { SnakeCaseNamingStrategy } from '@adonisjs/lucid/build/src/Orm/NamingStrategies/SnakeCase.js';
-import { scope } from '@adonisjs/lucid/build/src/Helpers/scope.js';
-import type { DatabaseConfig } from '@ioc:Adonis/Lucid/Database';
-import type { LucidModel } from '@ioc:Adonis/Lucid/Orm';
+import { Emitter } from '@adonisjs/events'
+import { Database } from '@adonisjs/lucid/database'
+import { BaseModel } from '@adonisjs/lucid/orm'
+import { Logger } from "@adonisjs/logger"
+import { Application } from '@adonisjs/application'
+import { Adapter } from '../node_modules/@adonisjs/lucid/build/src/orm/adapter/index.js'
+import { type DatabaseConfig } from '@adonisjs/lucid/types/database'
+import { LucidModel } from '@adonisjs/lucid/types/model'
 
-const appRoot = '';
 const logger = new Logger({
   name: 'app-name',
   enabled: true,
   level: 'info',
-  prettyPrint: true,
 });
-const profiler = new Profiler(appRoot, logger, {
-  enabled: true,
-  blacklist: [],
-  whitelist: [],
-});
-const emitter = new Emitter();
-const ioc = new Ioc();
+const application = new Application(
+  new URL('./', import.meta.url),
+  { environment: 'web' }
+);
+const emitter = new Emitter(application);
 
 export function defineConfig(config: DatabaseConfig) {
-  const database = new Database(config, logger, profiler, emitter);
+  const database = new Database(config, logger, emitter);
   BaseModel.$adapter = new Adapter(database);
-  BaseModel.$container = ioc;
   return {
     Database: database,
-    BaseModel: BaseModel as LucidModel,
     Event: emitter,
+    BaseModel: BaseModel as LucidModel,
   };
 }
-
-export * from '@adonisjs/lucid/build/src/Orm/Decorators/index.js';
-export { ModelPaginator, SnakeCaseNamingStrategy, scope };
