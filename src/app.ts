25f7fd99aca51d04b27a60931bf83c79552ef3cf
Gideão Silva <gideaoms@gmail.com>
2024-11-03 11:33:55 -0300

    format code

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
-import { Emitter } from '@adonisjs/events'
-import { Database } from '@adonisjs/lucid/database'
-import { BaseModel } from '@adonisjs/lucid/orm'
-import { Logger } from "@adonisjs/logger"
-import { Application } from '@adonisjs/application'
-import { Adapter } from '../node_modules/@adonisjs/lucid/build/src/orm/adapter/index.js'
-import { type DatabaseConfig } from '@adonisjs/lucid/types/database'
-import { LucidModel } from '@adonisjs/lucid/types/model'
+import { Emitter } from '@adonisjs/events';
+import { Database } from '@adonisjs/lucid/database';
+import { BaseModel } from '@adonisjs/lucid/orm';
+import { Logger } from '@adonisjs/logger';
+import { Application } from '@adonisjs/application';
+import { Adapter } from '../node_modules/@adonisjs/lucid/build/src/orm/adapter/index.js';
+import { type DatabaseConfig } from '@adonisjs/lucid/types/database';
+import { LucidModel } from '@adonisjs/lucid/types/model';
 
 const logger = new Logger({
   name: 'app-name',
   enabled: true,
   level: 'info',
 });
-const application = new Application(
-  new URL('./', import.meta.url),
-  { environment: 'web' }
-);
+const application = new Application(new URL('./', import.meta.url), {
+  environment: 'web',
+});
 const emitter = new Emitter(application);
 
 export function defineConfig(config: DatabaseConfig) {
